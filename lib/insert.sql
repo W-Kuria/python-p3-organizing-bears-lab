773f3e700aacaa87e0c33117300bd4e4ac3b244b
W-Kuria <william.njoroge.kuria@gmail.com>
2025-09-02 14:52:18 +0300

    feat: Complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 4, 'F', 'Golden', 'Friendly', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Strict', 1),
+('Melissa', 7, 'F', 'White', 'Gentle', 1),
+('Grinch', 5, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 6, 'F', 'Red', 'Curious', 1),
+(NULL, 3, 'M', 'Gray', 'Mysterious', 1);
